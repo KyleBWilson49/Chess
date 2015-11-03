146a51a34eef252da0513c2dc51e7136c3748f06
Kyle <kylebwilson49@gmail.com>
2015-11-03 14:02:33 -0800

    refactored pawn

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -10,20 +10,26 @@ class Pawn < Piece
     super
   end
 
-
   def moves
-    valid_moves = []
     grid = @board.grid
     x,y = position
-
     pawn_moves = [[1,0]]
     pawn_attacks = [[1,1],[1,-1]]
 
-    if (x == 1 && color == :black) || (x == 6 && color == :white)
+    unless moved?(x, color)
       pawn_moves << [2,0]
     end
 
-    pawn_moves.each do |move|
+    advance(pawn_moves, x, y, grid) + attacks(pawn_attacks, x, y, grid)
+  end
+
+  def moved?(x, color)
+    !(x == 1 && color == :black) && !(x == 6 && color == :white)
+  end
+
+  def advance(moves, x, y, grid)
+    valid_moves = []
+    moves.each do |move|
       dx, dy = move
       dx *= -1 if color == :white
       space = [x + dx, y + dy]
@@ -33,13 +39,17 @@ class Pawn < Piece
         end
       end
     end
+    valid_moves
+  end
 
-    pawn_attacks.each do |move|
+  def attacks(moves, x, y, grid)
+    valid_moves = []
+    moves.each do |move|
       dx, dy = move
       dx *= -1 if color == :white
       space = [x + dx, y + dy]
-      if @board.in_bounds?(space) && !grid[space[0]][space[1]].nil?
-        if grid[space[0]][space[1]].color != self.color
+      if @board.in_bounds?(space)
+        if grid[space[0]][space[1]].nil?
           valid_moves << space
         end
       end
