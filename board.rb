87b2972a5f7c2add92c64bd31abea39f9b52973d
Kyle <kylebwilson49@gmail.com>
2015-11-03 14:10:18 -0800

    refactored place_pieces

--- board.rb
+++ board.rb
@@ -10,32 +10,11 @@ class Board
   end
 
   def place_pieces
-    @grid[0][0] = Rook.new(:black, [0,0], self)
-    @grid[0][7] = Rook.new(:black, [0,7], self)
-    @grid[7][0] = Rook.new(:white, [7,0], self)
-    @grid[7][7] = Rook.new(:white, [7,7], self)
-
-    @grid[0][2] = Bishop.new(:black, [0,2], self)
-    @grid[0][5] = Bishop.new(:black, [0,5], self)
-    @grid[7][2] = Bishop.new(:white, [7,2], self)
-    @grid[7][5] = Bishop.new(:white, [7,5], self)
-
-    @grid[0][3] = Queen.new(:black, [0,3], self)
-    @grid[7][3] = Queen.new(:white,[7,3], self)
-
-    @grid[0][4] = King.new(:black, [0,4], self)
-    @grid[7][4] = King.new(:white, [7,4], self)
-
-    @grid[0][1] = Knight.new(:black, [0,1], self)
-    @grid[0][6] = Knight.new(:black, [0,6], self)
-    @grid[7][1] = Knight.new(:white, [7,1], self)
-    @grid[7][6] = Knight.new(:white, [7,6], self)
-
-    (0..7).each do |col|
+    starting_pieces = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+    starting_pieces.each_with_index do |piece, col|
+      @grid[0][col] = piece.new(:black, [0,col], self)
+      @grid[7][col] = piece.new(:white, [7,col], self)
       @grid[1][col] = Pawn.new(:black, [1,col], self)
-    end
-
-    (0..7).each do |col|
       @grid[6][col] = Pawn.new(:white, [6,col], self)
     end
   end
