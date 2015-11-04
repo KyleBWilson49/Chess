c7a412222d79aecdfd5a45dda6873f1131edfca0
Kyle <kylebwilson49@gmail.com>
2015-11-03 18:05:23 -0800

    AI is missed up

--- computerplayer.rb
+++ computerplayer.rb
@@ -13,23 +13,56 @@ class ComputerPlayer
   end
 
   def play_turn
-    # debugger
     moves_hash = available_moves
     if @has_selected == nil
-      possible_pieces = moves_hash.select { |key, val| !val.empty? }
+      possible_pieces = select_pieces(moves_hash)
       @has_selected = possible_pieces.keys.sample
     else
-      new_move = moves_hash[@has_selected].sample
+      new_move = attack(moves_hash[@has_selected])
       @has_selected = nil
       new_move
     end
+  end
 
+  def attack(moves)
+    attack_moves = moves.select { |move| enemy_positions.include?(move) }
+    return attack_moves.sample unless attack_moves.empty?
+    moves.sample
+  end
+
+  def select_pieces(moves_hash)
+    debugger
+    available_pieces = moves_hash.select { |key, val| !val.empty? }
+
+    attack_pieces = available_pieces.select do |key, val|
+
+
+      # val.each { |val| enemy_positions.include?(val) }
+    end
+
+    return attack_pieces unless attack_pieces.empty?
+    available_pieces
+  end
+
+  def friend_pieces
+    @board.grid.flatten.select { |piece| !piece.nil? && piece.color == @color }
+  end
+
+  def friend_positions
+    friend_pieces.map { |piece| piece.position }
+  end
+
+  def enemy_pieces
+    @board.grid.flatten.select { |piece| !piece.nil? && piece.color != @color }
+  end
+
+  def enemy_positions
+    enemy_pieces.map { |piece| piece.position }
   end
 
   def available_moves
-    pieces = @board.grid.flatten.select { |piece| !piece.nil? && piece.color == @color }
     moves_hash = Hash.new { |h,k| h[k] = [] }
-    pieces.each do |piece|
+    friend_pieces.each do |piece|
       moves_hash[piece.position] = piece.valid_moves
     end
     moves_hash
